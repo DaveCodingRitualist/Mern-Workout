597a460a7203dbe1b01312e725bf82a94d8a61a8
Dave Lafortune <davefortune527@gmail.com>
2025-05-23 19:40:28 +0200

    Auth context added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 const cors = require('cors')
 // express app
 const app = express()
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
